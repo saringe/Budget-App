1aa87cc2fd99c712d27d1c01de5ae98adba3031f
Cliff Saringe <saringecliff@gmail.com>
2020-04-27 16:53:54 +0300

    Getting input data

--- app.js
+++ app.js
@@ -6,15 +6,37 @@ const budgetController = (function (){
 
 const UIcontroller = (function (){
 
+    var DOMStrings = {
+        inputType : '.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        inputButton : '.add__btn'
+    }
 
+    return {
+        getInput : function (){
+            return {
+                 type : document.querySelector(DOMStrings.inputType).value,
+                 description : document.querySelector(DOMStrings.inputDescription).value,
+                 value : document.querySelector(DOMStrings.inputValue).value
+            }
+           
+        }, 
+        getDOMStrings : function (){
+            return DOMStrings;
+        }
+    }
+
+  
 
 })();
-
+ 
 const appController = (function (budgetCtrl, UICtrl){
-
+    var DOM = UICtrl.getDOMStrings();
     var AddCtrlItem = function () {
          // 1. Get the input data
-        
+                var input = UICtrl.getInput();
+                console.log(input);
         // 2. update the input item to the budget controller
 
         // 3. update the input item to the UI
@@ -22,10 +44,10 @@ const appController = (function (budgetCtrl, UICtrl){
         // 4. calculate the budget
 
         // 5. display the calculated budget
-        console.log("works")
+       
     }
 
-   document.querySelector('.add__btn').addEventListener('click', AddCtrlItem);
+   document.querySelector(DOM.inputButton).addEventListener('click', AddCtrlItem);
 
    document.addEventListener('keypress', function (event){
 
