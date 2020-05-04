7becf52922098f7b865928b18e4f93b0e363387d
Cliff Saringe <saringecliff@gmail.com>
2020-05-04 13:02:45 +0300

    Adding list Items

--- app.js
+++ app.js
@@ -64,7 +64,10 @@ const UIcontroller = (function (){
         inputType : '.add__type',
         inputDescription : '.add__description',
         inputValue : '.add__value',
-        inputButton : '.add__btn'
+        inputButton : '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
+
     }
 
     return {
@@ -76,6 +79,32 @@ const UIcontroller = (function (){
             }
            
         }, 
+        addListItem : function (obj, type){
+            var html, newHtml, element;
+
+            // Create HTML with a place holder
+
+            if (type === 'inc'){
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+
+            }else if(type === 'exp'){
+                element = DOMStrings.expensesContainer;
+
+                html =  '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>'
+            }
+
+            // Replace the placeHolder with input data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+
+        },
+
         getDOMStrings : function (){
             return DOMStrings;
         }
@@ -112,7 +141,7 @@ const appController = (function (budgetCtrl, UICtrl){
         // 2. update the input item to the budget controller
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         // 3. update the input item to the UI
-
+        UICtrl.addListItem(newItem, input.type);
         // 4. calculate the budget
 
         // 5. display the calculated budget
